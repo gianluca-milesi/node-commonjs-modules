671302274115f818c5d994260ddfd4e257f92f82
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-11-16 16:55:03 +0100

    ADD: second step. createHobbies function

---
+++ functions/hobbies.js
@@ -0,0 +1,5 @@
+function createHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return hobby = [hobbyOne, hobbyTwo, hobbyThree];
+};
+
+module.exports = createHobbies;
--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const createPerson = require("./functions/names.js");
+const createHobbies = require("./functions/hobbies.js");
 
+// Step 1.
 const person = createPerson("Gianluca", "Milesi");
 console.log(person);
+
+//Step 2.
+const hobbies = createHobbies("Suonare la chitarra", "Allenarsi", "Uscire con amici");
+console.log(hobbies);
